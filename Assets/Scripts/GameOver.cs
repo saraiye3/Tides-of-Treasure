4e3f473c216fb2ea143209d1a16481e3afa02380
saraiye3 <sarai171298@gmail.com>
2025-09-14 19:35:57 +0300

    new bg + return button + replaced buttons

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,83 +5,85 @@ using TMPro;
 
 
 public class GameOver : MonoBehaviour
+{
+    public GameObject screenParent;
+    public GameObject scoreParent;
+    public TMP_Text loseText;
+    public TMP_Text scoreText;
+    public UnityEngine.UI.Image[] stars;
+    public Animator animator;
+
+    public int currentStageIndex = 1;
+
+    private void Start()
     {
-        public GameObject screenParent;
-        public GameObject scoreParent;
-        public TMP_Text loseText;
-        public TMP_Text scoreText;
-        public UnityEngine.UI.Image[] stars;
-        public Animator animator;
+        screenParent.SetActive(false);
 
-        public int currentStageIndex =  1;
-
-        private void Start()
+        for (int i = 0; i < stars.Length; i++)
         {
-            screenParent.SetActive(false);
-
-            for (int i = 0; i < stars.Length; i++)
-            {
-                stars[i].enabled = false;
-            }
-        }
-
-        public void ShowLose()
-        {
-            screenParent.SetActive(true);
-            scoreParent.SetActive(false);
-
-            if (animator)
-            {
-                animator.Play("GameOverShow");
-            }
-        }
-
-        public void ShowWin(int score, int starCount)
-        {
-            screenParent.SetActive(true);
-            loseText.enabled = false;
-
-            scoreText.text = score.ToString();
-            scoreText.enabled = false;
-
-            if (animator)
-            {
-                animator.Play("GameOverShow");
-            }
-
-            StartCoroutine(ShowWinCoroutine(starCount));
-        }
-
-        private IEnumerator ShowWinCoroutine(int starCount)
-        {
-            yield return new WaitForSeconds(0.5f);
-
-            if (starCount < stars.Length)
-            {
-                for (int i = 0; i <= starCount; i++)
-                {
-                    stars[i].enabled = true;
-
-                    if (i > 0)
-                    {
-                        stars[i - 1].enabled = false;
-                    }
-
-                    yield return new WaitForSeconds(0.5f);
-                }
-            }
-
-            scoreText.enabled = true;
-        }
-
-        public void OnReplayClicked()
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
-        }
-
-        public void OnDoneClicked()
-        {
-            LevelProgress.UnlockUpTo(currentStageIndex + 1);// open the next level
-            UnityEngine.SceneManagement.SceneManager.LoadScene("menuScene");
+            stars[i].enabled = false;
         }
     }
+
+    public void ShowLose()
+    {
+        screenParent.SetActive(true);
+        scoreParent.SetActive(false);
+
+        if (animator)
+        {
+            animator.Play("GameOverShow");
+        }
+    }
+
+    public void ShowWin(int score, int starCount)
+    {
+        screenParent.SetActive(true);
+        loseText.enabled = false;
+
+        scoreText.text = score.ToString();
+        scoreText.enabled = false;
+
+        if (animator)
+        {
+            animator.Play("GameOverShow");
+        }
+
+        StartCoroutine(ShowWinCoroutine(starCount));
+    }
+
+    private IEnumerator ShowWinCoroutine(int starCount)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (starCount < stars.Length)
+        {
+            for (int i = 0; i <= starCount; i++)
+            {
+                stars[i].enabled = true;
+
+                if (i > 0)
+                {
+                    stars[i - 1].enabled = false;
+                }
+
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+
+        scoreText.enabled = true;
+    }
+
+
+    public void OnReplayClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("level1");
+    }
+
+
+    public void OnDoneClicked()
+    {
+        LevelProgress.UnlockUpTo(currentStageIndex + 1);// open the next level
+        UnityEngine.SceneManagement.SceneManager.LoadScene("menuScene");
+    }
+}
